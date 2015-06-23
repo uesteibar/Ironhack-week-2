cb627820682bc132cf3d7876463b22bd48c80080
uesteibar <uesteibar@gmail.com>
2015-06-23 10:41:09 +0200

    sinatra sessions v2

--- 2-tuesday/1-sessions/main.rb
+++ 2-tuesday/1-sessions/main.rb
@@ -9,11 +9,11 @@ set :bind, "0.0.0.0"
 
 enable :sessions
 
+
 id_manager = IdManager.new(0)
 users = {}
 
 get '/' do
-
   if session[:user_id]
     @username = users[session[:user_id]]
     @users = users
@@ -27,5 +27,20 @@ post '/signup' do
   users[user_id] = params[:username]
   session[:user_id] = user_id
 
+  redirect to('/')
+end
+
+get '/cats' do
+  if users[session[:user_id]]
+    erb :cats
+  else
+    redirect to('/')
+  end
+end
+
+delete '/signout' do
+  if users[session[:user_id]]
+      users.delete(session[:user_id])
+  end
   redirect to('/')
 end

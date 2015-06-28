85ab3c26a6dfac8d478b4d0ab792dbc1a736132c
uesteibar <uesteibar@gmail.com>
2015-06-28 20:08:17 +0200

    set conditions using lambda

--- 6-bonus/1-insecure-passwords/model/password_checker.rb
+++ 6-bonus/1-insecure-passwords/model/password_checker.rb
@@ -1,22 +1,31 @@
 
 class PasswordChecker
+  def initialize
+    @password_conditions = [
+      ->(email, password) { password.size >= 8 },
+      ->(email, password) { password =~ /\d/ },
+      ->(email, password) { password =~ /[A-Z]/ },
+      ->(email, password) { password =~ /[a-z]/ },
+      ->(email, password) {
+        special = "?<>',?[]}{=-)(*&^%$#`~{}"
+        regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+        password =~ regex
+      },
+      ->(email, password) {
+        username = email.slice(0..email.index("@")-1)
+        !password.downcase.include?(username.downcase)
+      },
+      ->(email, password) {
+        after_at = email.slice(email.index("@")+1..-1)
+        domain = after_at.slice(0..after_at.index(".")-1)
+        !password.downcase.include?(domain.downcase)
+      }
+    ]
+  end
   def check_password(email, password)
-    return false if password.size < 8
-    return false if !(password =~ /\d/)
-    return false if !(password =~ /[A-Z]/)
-    return false if !(password =~ /[a-z]/)
-
-    special = "?<>',?[]}{=-)(*&^%$#`~{}"
-    regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
-    return false if !(password =~ regex)
-
-    username = email.slice(0..email.index("@")-1)
-    after_at = email.slice(email.index("@")+1..-1)
-    domain = after_at.slice(0..after_at.index(".")-1)
-
-    return false if password.downcase.include?(username.downcase)
-    return false if password.downcase.include?(domain.downcase)
-
+    @password_conditions.each do |condition|
+      return false if !condition.call(email, password)
+    end
     return true
   end
 end
